0573ebd7bac369004b7064c38130185a2dc95537
Tomasz Chybzinski <tomasz.chybzinski@gmail.com>
2021-01-21 13:58:35 +0100

    extract Movies component

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,27 +1,15 @@
-import React, { FC, useEffect, useState } from "react";
+import React, { FC } from "react";
 import logo from "../../assets/LOGO.svg";
 import "./App.scss";
-import { IMovie } from "../../api/movie/types";
-import Movie from "../movie/Movie";
 import Container from "../shared/container/Container";
-import { getMovies } from "../../api/movie/movie";
+import Movies from "../movies/Movies";
 
 const App: FC = () => {
-	const [movies, setMovies] = useState<IMovie[]>([]);
-
-	useEffect(() => {
-		getMovies().then((movies) => {
-			setMovies(movies || []);
-		});
-	}, []);
-
 	return (
 		<div className="app">
 			<Container>
 				<img src={logo} alt="logo" className="logo" />
-				{movies.map(({ title, planets }) => (
-					<Movie key={title} title={title} planetUrls={planets} />
-				))}
+				<Movies />
 			</Container>
 		</div>
 	);
---
+++ src/components/movies/Movies.tsx
@@ -0,0 +1,24 @@
+import React, { FC, useEffect, useState } from "react";
+import { getMovies } from "../../api/movie/movie";
+import { IMovie } from "../../api/movie/types";
+import Movie from "../movie/Movie";
+
+const Movies: FC = () => {
+	const [movies, setMovies] = useState<IMovie[]>([]);
+
+	useEffect(() => {
+		getMovies().then((movies) => {
+			setMovies(movies || []);
+		});
+	}, []);
+
+	return (
+		<>
+			{movies.map(({ title, planets }) => (
+				<Movie key={title} title={title} planetUrls={planets} />
+			))}
+		</>
+	);
+};
+
+export default Movies;
