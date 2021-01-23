a73911d3ab9f1904337cd1137351ecc328949325
Tomasz Chybzinski <tomasz.chybzinski@gmail.com>
2021-01-23 19:08:24 +0100

    extract shared Card component

--- src/components/shared/collapsibleCard/CollapsibleCard.tsx
+++ src/components/shared/collapsibleCard/CollapsibleCard.tsx
@@ -2,6 +2,7 @@ import React, { FC } from "react";
 import "./CollapsibleCard.scss";
 import arrowOpen from "../../../assets/ARROW OPEN.svg";
 import arrowClose from "../../../assets/ARROW CLOSE.svg";
+import Card from "../card/Card";
 
 interface IProps {
 	onClick: () => void;
@@ -12,7 +13,7 @@ interface IProps {
 const CollapsibleCard: FC<IProps> = ({ onClick, isOpen, title, children }) => {
 	return (
 		<div className="collapsible-card">
-			<div className="card">
+			<Card>
 				<span className="title">
 					<p>{title}</p>
 				</span>
@@ -21,7 +22,7 @@ const CollapsibleCard: FC<IProps> = ({ onClick, isOpen, title, children }) => {
 						<img src={isOpen ? arrowClose : arrowOpen} alt="arrowButton" />
 					</button>
 				</span>
-			</div>
+			</Card>
 			{isOpen ? <div className="card open">{children}</div> : null}
 		</div>
 	);
